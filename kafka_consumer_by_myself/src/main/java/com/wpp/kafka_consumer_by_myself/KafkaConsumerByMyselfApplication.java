d9714882c4b7123a3093c766f0a6682d61310827
王培鹏 <wangpeipeng123@163.com>
2024-10-14 16:03:44 +0800

    kafak 依赖提交

---
+++ kafka_consumer_by_myself/src/main/java/com/wpp/kafka_consumer_by_myself/KafkaConsumerByMyselfApplication.java
@@ -0,0 +1,13 @@
+package com.wpp.kafka_consumer_by_myself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerByMyselfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaConsumerByMyselfApplication.class, args);
+    }
+
+}
---
+++ kafka_consumer_by_myself/src/test/java/com/wpp/kafka_consumer_by_myself/KafkaConsumerByMyselfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wpp.kafka_consumer_by_myself;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConsumerByMyselfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
